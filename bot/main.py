c40a1b74d1480b828978da4d6bc0a5ba5795d695
arkady <arksap2002@gmail.com>
2021-08-03 06:02:37 +0300

    bad buttons

--- bot/main.py
+++ bot/main.py
@@ -1,146 +1,96 @@
-import logging
-
-from parser import get_topics, get_href
-from config import TG_TOKEN
-
-from telegram import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    Update,
-    ReplyKeyboardMarkup,
-    ReplyKeyboardRemove,
-    ParseMode
-)
-
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-    CallbackQueryHandler,
-    ConversationHandler,
-    CallbackContext,
-    MessageHandler,
-    Filters,
-)
-
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-
-# Stages
-MAIN, GET, EXIT = range(3)
-# Callback data
-CMAIN, CGET, smth1, CEXIT = range(4)
-#dict of tops with links
-
-def start(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    logger.info("User %s started the conversation.", user.first_name)
-
-    keyboard = [
-        [
-            InlineKeyboardButton("theme", callback_data=str(CGET)),
-        ],
-
-        [
-            InlineKeyboardButton("exit", callback_data=str(CEXIT)),
-        ],
-    ]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("Start handler, Choose a route", reply_markup=reply_markup)
-
-    return MAIN
-
-def start_over(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    logger.info("User %s started_over the conversation.", user.first_name)
-
-    keyboard = [
-        [
-            InlineKeyboardButton("theme", callback_data=str(CGET)),
-        ],
-
-        [
-            InlineKeyboardButton("exit", callback_data=str(CEXIT)),
-        ],
-    ]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("Start handler, Choose a route", reply_markup=reply_markup)
-
-    return MAIN
+# imports
+from telegram import (Bot,
+                      Update,
+                      InlineKeyboardButton,
+                      InlineKeyboardMarkup)
+from telegram.ext import (Updater,
+                          CommandHandler,
+                          MessageHandler,
+                          Filters,
+                          CallbackQueryHandler)
+from bot.config import TG_TOKEN
 
 
-def get(update: Update, context: CallbackContext) -> int:
-    text = update.message.text
-    user = update.message.from_user
+# buttons inits
+CALLBACK_BUTTON_SPORTS = "callback_button_sports"
+CALLBACK_BUTTON_POLITICS = "callback_button_politics"
+CALLBACK_BUTTON_OTHER = "callback_button_other"
 
-    logger.info("User %s in get func, got text: %s", user.first_name, text)
 
-    reply_keyboard = [["1", "2", "3"]]
+# list of buttons
+TITLES = {
+    CALLBACK_BUTTON_SPORTS: "Sports",
+    CALLBACK_BUTTON_POLITICS: "Politics",
+    CALLBACK_BUTTON_OTHER: "Other"
+}
 
-    tops = get_topics(text)
-    text = "choose one of this topics:\n1)" + get_href(tops[0]) + "\n2)" + get_href(tops[1]) + "\n3)" + get_href(tops[2])
 
-    update.message.reply_text(text,
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder='Number of topic?',
-            parse_mode=ParseMode.HTML
-        ),
+# start move
+def do_start(bot: Bot, update: Update):
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Choose the topic",
+        reply_markup=get_start_keyboard(),
     )
 
-    return MAIN
 
-def pre_get(update: Update, context: CallbackContext) -> int:
+# start keyboard init
+def get_start_keyboard():
+    keyboard = [
+        [
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON_SPORTS], callback_data=CALLBACK_BUTTON_SPORTS),
+            InlineKeyboardButton(TITLES[CALLBACK_BUTTON_SPORTS], callback_data=CALLBACK_BUTTON_SPORTS),
+        ],
+        InlineKeyboardButton(TITLES[CALLBACK_BUTTON_OTHER], callback_data=CALLBACK_BUTTON_OTHER),
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+
+# processing of the start keyboard
+def keyboard_processing(bot: Bot, update: Update, chat_data=None, **kwargs):
     query = update.callback_query
-    user = update.callback_query.from_user
-    query.answer()
+    data = query.data
+    if data == CALLBACK_BUTTON_SPORTS:
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text="Sports news:\n"
+                 "todo!",  # TODO
+        )
+    if data == CALLBACK_BUTTON_POLITICS:
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text="Politics news:\n"
+                 "todo!",  # TODO
+        )
+    if data == CALLBACK_BUTTON_OTHER:
+        text = update.message.text
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text=text + "news:\n"
+                        "todo!",  # TODO
+        )
 
-    logger.info("User %s in pre_get func", user.first_name)
 
-    query.edit_message_text(text="Enter theme pls:")
-    return GET
-
-def exit_(update: Update, context: CallbackContext) -> int:
-    query = update.callback_query
-    user = update.callback_query.from_user
-    query.answer()
-
-    logger.info("User %s in exit func", user.first_name)
-
-    return MAIN
+# parsing call
+def find_news(topic):
+    topic_tmp = topic  # TODO Slava's job here in the other file, i guess
 
 
 def main():
-    updater = Updater(TG_TOKEN)
-    dispatcher = updater.dispatcher
+    bot = Bot(token=TG_TOKEN, )
+    updater = Updater(bot=bot, )
 
-    conv_handler = ConversationHandler(
-            entry_points=[CommandHandler("start", start)],
-            states={
-                MAIN: [
-                        CallbackQueryHandler(start_over, pattern='^' + str(CMAIN) + '$'),
-                        CallbackQueryHandler(pre_get, pattern='^' + str(CGET) + '$'),
-                        CallbackQueryHandler(exit_, pattern='^' + str(CEXIT) + '$'),
-                    ],
-                GET: [
-                        MessageHandler(
-                            Filters.text & ~(Filters.command | Filters.regex('^Done$')),
-                            get,
-                        )
-                    ],
-                EXIT: [
-                    ],
-                },
-            fallbacks=[CommandHandler("start", start)],
-        )
+    start_handler = CommandHandler("start", do_start)
+    buttons_handler = CallbackQueryHandler(callback=keyboard_processing, pass_chat_data=True)
 
-    dispatcher.add_handler(conv_handler)
+    updater.dispatcher.add_handler(start_handler)
+    updater.dispatcher.add_handler(buttons_handler)
 
+    # start input
     updater.start_polling()
+    # do not exit too early
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
