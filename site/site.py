3a47124881c2dc6b8d53391d19975e7041cc54db
Pudums <max_mail2002@bk.ru>
2021-08-04 21:16:20 +0300

    site base

---
+++ site/site.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+#from app import app
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
+
+if __name__ == "__main__":
+    app.run(debug=True)
