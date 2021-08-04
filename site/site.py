0c8e0a54f6e4dcbf9564bab86a2b9b2c73438b64
Pudums <max_mail2002@bk.ru>
2021-08-04 22:04:18 +0300

    save

--- site/site.py
+++ site/site.py
@@ -8,8 +8,18 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    #user = {'username': 'Miguel'}
+    tops = \
+    [
+        {
+            "title": "TITLE",
+            "time": "9 mins",
+        },
+        {
+            "title": "Check no time",
+        },
+    ]
+    return render_template('index.html', title='Home', tops=tops,)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
