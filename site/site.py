d3f70186e506f834bb61bcd023d92d8973bd1b4a
Pudums <max_mail2002@bk.ru>
2021-08-06 20:32:53 +0300

    login test

--- site/site.py
+++ site/site.py
@@ -1,31 +1,37 @@
-import sys, os
-
-sys.path.insert(1, os.getcwd() + '/../')
-print(sys.path)
-
 from flask import (
     Flask, 
     render_template, 
     request,
     session,
+    flash,
 )
 
 from flask_login import (
     login_user, 
     logout_user, 
     current_user, 
-    login_required
+    login_required,
+    login_manager,
+    LoginManager,
 )
 
-from loading import *
+from data_processing.loading import *
 
 
 app = Flask(__name__)
+app.secret_key = 'some key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return cur_users[user_id] if user_id in all_users.keys() else None
+    #return User.get(user_id)
 
 
 @app.route('/')
 def index():
-    print(type(session))
     tops = \
     [
         {
@@ -59,14 +65,30 @@ def login():
     email = request.form['email']
     pswd = request.form['password']
 
+    add_to_current_or_create_user(email)
+    user = cur_users[email]
 
-
+    if login_user(user, remember=True):
+        print("Logged in!")
+        #return redirect('/notes/create')
+    else:
+        print("unable to log you in")
     return index()
 
 
 @app.route('/test')
 def test():
-    return render_template('test.html')
+    print(type(current_user))
+    print(current_user)
+    try:
+        print(current_user.is_authenticated())
+    except:
+        print("func faill")
+    try:
+        print(current_user.is_authenticated)
+    except:
+        print("peremennaya faill")
+    return render_template('test.html', user=current_user if current_user.is_authenticated else None)
 
 
 @app.route('/sing-up')
