41bf44241be5c994344ff7040c9a3756726758b7
DymashevskijSP <sp.dymashevskij@gmail.com>
2021-08-14 12:25:09 +0300

    changed metrics

--- data_processing/neural.py
+++ data_processing/neural.py
@@ -15,17 +15,16 @@ class Neural:
         self.samples = []
         self.grades = []
     def partial_fit(self, article_info: Article, grade: float):
-        # sample = np.array([article_info.likes, article_info.comments, article_info.size, article_info.image_count])
-        sample = [article_info.size]
+        sample = np.array([article_info.likes, article_info.comments, article_info.size, article_info.image_count])
+        #sample = [article_info.size]
         self.samples.append(sample)
         self.grades.append(grade)
     def predict(self, article_info: Article):
-        # sample = np.array([article_info.likes, article_info.comments, article_info.size, article_info.image_count])
+        sample = np.array([article_info.likes, article_info.comments, article_info.size, article_info.image_count])
         if (len(self.samples) > 0):
             self.regressor.fit(self.samples, self.grades)
-        sample = [article_info.size]
-        print("predict", sample)
-        return self.regressor.predict([sample])
+            global_model.regressor.fit(self.samples, self.grades)
+        return self.regressor.predict(sample)
 
     def get_best_topics(self, articles):
         grades = []
@@ -43,3 +42,7 @@ def train():
         size = random.randint(0, 6000)
         print(size, model.predict(Article(0, 0, size, 0, ""))[0])
 
+
+global_model = Neural()
+def global_predict(article: Article):
+    return global_model.predict(article)
